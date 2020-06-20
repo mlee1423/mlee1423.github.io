1eab5049cd93b5f553a7b6632b9e60a65501326a
Meghann Lee <lee.meghann@gmail.com>
2020-06-19 23:59:00 -0500

    landing page 'complete'
    - changed color scheme
    - added skills, projects, and contact to index.html
    - created about.html with placeholder text
    -added vjsweather project
    -added mobile responsiveness

--- js/landingpage.js
+++ js/landingpage.js
@@ -76,7 +76,7 @@ const textCarousel = () =>{
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #7D88BA }";
+        css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #F3F5F5 }";
         document.body.appendChild(css);
       };
 }
